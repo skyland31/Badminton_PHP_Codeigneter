20e056c94f89adf4b0089a61a9511186464accbd
KanashiXI <panyapat.test@gmail.com>
2020-03-19 20:19:59 +0700

    Show list competition

--- application/views/staff/competition/content.php
+++ application/views/staff/competition/content.php
@@ -36,6 +36,8 @@
               <br>
               <strong> สิ้นสุดการแข่งขัน : </strong><p class="detail" id="compet-end">10/04/2020</p>              
               <br>
+              <strong> สถานที่แข่งขัน : </strong><p class="detail" id="compet-end">10/04/2020</p>              
+              <br>
               <strong> เปิดรับสมัคร : </strong><p class="detail" id="start">12/03/2020</p>
               <br>
               <strong> ปิดรับสมัคร : </strong><p class="detail" id="end">22/03/2020</p>
@@ -56,18 +58,32 @@
         <div class="col-md-6">
           <div class="box">
             <div class="box-header border-box-bottom" id="compet-name">
-              <h3>ประดู่เกม 2563</h3>
+              <h3>Walailak's Day</h3>
             </div>
             <div class="box-body" id="compet-detail">
+              <strong> เปิดรับ : </strong>
+                <span class="label label-info">U9</span>
+                <span class="label label-info">U11</span>
+                <span class="label label-info">U13</span>
+                <span class="label label-info">U15</span>
+                <span class="label label-info">U17</span>
+                <span class="label label-success">N</span>
+                <span class="label label-success">S-</span>
+                <span class="label label-success">S+</span>
+                <span class="label label-success">P-</span>
+                <span class="label label-success">P+C</span>
+              <br>
               <strong> เริ่มการแข่งขัน : </strong><p class="detail" id="compet-start">05/04/2020</p> 
               <br>
               <strong> สิ้นสุดการแข่งขัน : </strong><p class="detail" id="compet-end">10/04/2020</p>              
+              <br>
+              <strong> สถานที่แข่งขัน : </strong><p class="detail" id="compet-end">10/04/2020</p>              
               <br>
               <strong> เปิดรับสมัคร : </strong><p class="detail" id="start">12/03/2020</p>
               <br>
               <strong> ปิดรับสมัคร : </strong><p class="detail" id="end">22/03/2020</p>
               <br>
-              <strong> ค่าสมัคร : </strong><p class="detail" id="prize">0฿</p>
+              <strong> ค่าสมัคร : </strong><p class="detail" id="prize">600฿</p>
               <br>
               <strong> สิ้นสุดจ่ายค่าสมัคร : </strong><p class="detail" id="pay-end">25/03/2020</p>
               <br><br>
